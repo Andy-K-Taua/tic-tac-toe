8ff875954efd209e385a2818709b5c8b580f017b
Andy Kalasini-Taua <andykalasinitaua@gmail.com>
2020-11-18 13:11:53 +1100

    intro second container

--- sei/projects/tic-tac-toe/js/main.js
+++ sei/projects/tic-tac-toe/js/main.js
@@ -1,15 +1,17 @@
-//This is where we will be grabbing our html elements
-const statusDiv = document.querySelector('.status');
+
+
+// step13 This is where we will be grabbing our html elements
+// const statusDiv = document.querySelector('.status');
 const resetDiv = document.querySelector('.reset');
 const cellDivs = document.querySelectorAll('.game-cell');
 
-//These are the game variables
+//These are the game variables .. continues
 let gameIsLive = true;
 let xIsNext = true;
 let winner = null;
 
 
-//functions
+//functions step14 .. continues
 
 const checkGamesStatus = () => {
   const topLeft = cellDivs[0].classList[2]
@@ -26,12 +28,13 @@ const checkGamesStatus = () => {
 //Determining the winner
 
 //These are the event handlers
-const handleReset = (e) => {
-  console.log(e);
-};
+
+// const handleReset = (e) => {
+//   console.log(e);
+// };
 
 const handleCellClick = (e) => {
-  const classList = e.target.classList;
+  const classList = e.target.classList; //When console logging event (e) I found a 'target' attribute. Within the target attribute there is what you call a classList. This has all the classes for the element that is in question.
   const location = classList[1];
 
   if(classList[2] === 'x' || classList[2] === 'o'){
@@ -53,8 +56,9 @@ const handleCellClick = (e) => {
 
 
 // These are the event listeners
-resetDiv.addEventListener('click', handleReset);
+// resetDiv.addEventListener('click', handleReset);
 
 for (const cellDiv of cellDivs) {
   cellDiv.addEventListener('click', handleCellClick);
 }
+// This is a 'for of' loop - this will add EventListener to each of our elements. What happens is this will loop within the each of the elements within cellDivs and store it in cellDiv
