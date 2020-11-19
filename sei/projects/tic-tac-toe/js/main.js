895a1645a74adc89f11ac41bd0ed0a125265addb
Andy Kalasini-Taua <andykalasinitaua@gmail.com>
2020-11-19 13:49:34 +1100

    Game error fixed

--- sei/projects/tic-tac-toe/js/main.js
+++ sei/projects/tic-tac-toe/js/main.js
@@ -1,23 +1,23 @@
 
 const resetDiv = document.querySelector('.reset');
 
-const cellDivs = document.querySelectorAll('.game-cell');
+// const cellDivs = document.querySelectorAll('.game-cell');
 const $cellDivs = $('.game-cell');
 
 let status = true;
 
 const checkGamesStatus = function(player) {
   const topLeft = $cellDivs.eq(0).hasClass(player)
-  const topMiddle = cellDivs[1].classList[2]
-  const topRight = cellDivs[2].classList[2]
-  const middleLeft = cellDivs[3].classList[2]
-  const middleMiddle = cellDivs[4].classList[2]
-  const middleRight = cellDivs[5].classList[2]
-  const bottomLeft = cellDivs[6].classList[2]
-  const bottomMiddle = cellDivs[7].classList[2]
-  const bottomRight = cellDivs[8].classList[2]
+  const topMiddle = $cellDivs.eq(1).hasClass(player)
+  const topRight = $cellDivs.eq(2).hasClass(player)
+  const middleLeft = $cellDivs.eq(3).hasClass(player)
+  const middleMiddle = $cellDivs.eq(4).hasClass(player)
+  const middleRight = $cellDivs.eq(5).hasClass(player)
+  const bottomLeft = $cellDivs.eq(6).hasClass(player)
+  const bottomMiddle = $cellDivs.eq(7).hasClass(player)
+  const bottomRight = $cellDivs.eq(8).hasClass(player)
 
-const winMessage = document.querySelector("#winMessage")
+const $winMessage = $("#winMessage");
 
 //================== Across
 
@@ -25,40 +25,40 @@ const winMessage = document.querySelector("#winMessage")
     console.log(`${player} wins`);
     winMessage.innerHTML=(`${player} wins top row`)
   }
-  if (middleLeft === player && middleMiddle === player && middleRight === player){
+  if (middleLeft && middleMiddle && middleRight){
     console.log(`${player} wins`);
     winMessage.innerHTML=(`${player} wins middle row`)
   }
-  if (bottomLeft === player && bottomMiddle === player && bottomRight === player){
+  if (bottomLeft && bottomMiddle && bottomRight){
     console.log(`${player} wins`);
     winMessage.innerHTML=(`${player} wins bottom row`)
   }
 
 //=================== Down
 
-  if (topLeft === player && middleLeft === player && bottomLeft === player){
+  if (topLeft && middleLeft && bottomLeft){
     console.log(`${player} wins`);
     winMessage.innerHTML = `${player} wins left column`;
   }
-  if (topMiddle === player && middleMiddle === player && bottomMiddle === player){
+  if (topMiddle && middleMiddle && bottomMiddle){
     console.log(`${player} wins`);
     winMessage.innerHTML=(`${player} wins middle column`)
   }
-  if (topRight === player && middleRight === player && bottomRight === player){
+  if (topRight && middleRight && bottomRight){
     console.log(`${player} wins`);
     winMessage.innerHTML=(`${player} wins right column`)
   }
 
 //========== Diagonal from the topLeft
 
- if (topLeft === player && middleMiddle === player && bottomRight === player){
+ if (topLeft && middleMiddle && bottomRight){
    console.log(`${player} wins`);
    winMessage.innerHTML=(`${player} wins diagonal`)
  }
 
  // ========== Diagonal from topRight
 
- if (topRight === player && middleMiddle === player && bottomLeft === player){
+ if (topRight && middleMiddle && bottomLeft){
    console.log(`${player} wins`);
    winMessage.innerHTML=(`${player} wins diagonal`)
  }
@@ -91,8 +91,8 @@ const handleCellClick = (e) => {
 
 // resetDiv.addEventListener('click', handleReset);
 
-for (const cellDiv of cellDivs) {
+for (const cellDiv of $cellDivs) {
   cellDiv.addEventListener('click', handleCellClick);
-  console.log(cellDiv);
+
 }
 // This is a 'for of' loop - this will add EventListener to each of our elements. What happens is this will loop within the each of the elements within cellDivs and store it in cellDiv
